46258486b264fa19d465f6e7d7f6e4c65e576769
azhi <astzhe@gmail.com>
2014-03-06 16:13:19 +0300

    SR lab1&2

---
+++ 8sem/SR/lab1/lab1.R
@@ -0,0 +1,27 @@
+f <- function(lambda) function(t) lambda * exp(-lambda*t)
+F <- function(lambda) function(t) 1 - exp(-lambda * t)
+F2 <- function(lambda) function(t) integrate(f(lambda), lower=0, upper=t)$value
+m <- function(lambda) 1 / lambda
+D <- function(lambda) 1 / lambda ^ 2
+median <- function(lambda) - log(0.5) / lambda
+
+plot_samples <- function(f, samples, colors, xrange, yrange, title, legendpos) {
+  plot(xrange, yrange, type="n", main=title, ylab=title, xlab="t")
+  for (i in 1:length(samples))
+    plot(f(samples[i]), xrange[1], xrange[2], add=TRUE, col=colors[i])
+  legend_labels = sapply(samples, function(sample) paste("lambda=", sample, sep=""))
+  legend(legendpos, NULL, legend_labels, fill=colors[1:length(samples)])
+}
+
+lambdas <- c(3.0, 1.5, 1.0, 0.5, 0.2)
+colors <- c("black", "red", "blue", "green", "yellow")
+
+pdf("out.pdf")
+plot_samples(f, lambdas, colors, c(0,5), c(0,3), "f(t)", "topright")
+plot_samples(F, lambdas, colors, c(0,5), c(0,1), "F(t)", "bottomright")
+# plot_samples(F2, lambdas, colors, c(0,5), c(0,1), "F2(t)", "bottomright")
+plot(m, 0, 3, main="m(lambda)", ylab="m(lambda)", xlab="lambda")
+plot(D, 0, 3, main="D(lambda)", ylab="D(lambda)", xlab="lambda")
+plot(median, 0, 3, main="median(lambda)", ylab="median(lambda)", xlab="lambda")
+plot(c(0,5), type="n", main="assymetric(lambda)", ylab="assymetric(lambda)", xlab="lambda")
+abline(h=2.0)
---
+++ 8sem/SR/lab2/lab2.R
@@ -0,0 +1,32 @@
+N <- 100000
+lambda <- 1.5
+
+reverse_exp_function <- function(x) -1 / lambda * log(x)
+F <- function(t) 1 - exp(-lambda * t)
+
+sequence <- sapply(runif(N, 0, 1), reverse_exp_function)
+
+distribution_histogramm <- hist(sequence, breaks=20, plot=FALSE)
+freqs <- sapply(distribution_histogramm$counts, function(c) c / N)
+breaks <- distribution_histogramm$breaks
+break_diffs <- diff(breaks)
+th_freqs <- sapply(1:(length(breaks)-1), function(i) F(breaks[i+1]) - F(breaks[i]))
+
+pdf("out.pdf")
+break_labels <- breaks[2:length(breaks)]
+th_break_xs <- sapply(1:(length(breaks) - 1), function(i) breaks[i] + break_diffs[i] / 2 )
+barplot(freqs, names.arg=break_labels, width=break_diffs[1], space=0, main="Distribution histogramm", xlab="t", ylab="freq")
+lines(th_break_xs, th_freqs, col="red")
+
+m <- mean(sequence)
+D <- var(sequence)
+th_m <- 1 / lambda
+th_D <- 1 / lambda ^ 2
+
+plot(0:100, 0:100, type="n", axes=FALSE, xlab="", ylab="")
+text(20, 90, paste("m = ", m))
+text(70, 90, paste("th_m = ", th_m))
+text(40, 80, paste("dm = ", abs(th_m - m)))
+text(20, 50, paste("D = ", D))
+text(70, 50, paste("th_D = ", th_D))
+text(40, 40, paste("dD = ", abs(th_D - D)))
