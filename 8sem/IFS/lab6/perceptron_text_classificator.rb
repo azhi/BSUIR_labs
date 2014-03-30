780031609251b77a8610c74bcb48b1012bf0ae47
azhi <astzhe@gmail.com>
2014-03-30 18:30:06 +0300

    IFS lab6 basic version

---
+++ 8sem/IFS/lab6/perceptron_text_classificator.rb
@@ -0,0 +1,54 @@
+require File.join(File.dirname(__FILE__), '../lab1/zipf.rb')
+require File.join(File.dirname(__FILE__), '../../../5sem/MADM/lab4/perceptron.rb')
+require 'set'
+require 'pry'
+
+class PerceptronTextClassificator
+  attr_reader :classes, :perceptron
+
+  def initialize(classes, train_texts)
+    @classes = classes
+    @train_texts = train_texts
+    prepare_train_texts
+    @perceptron = build_perceptron
+  end
+
+  def recognize(text_filename)
+    language = text_filename.end_with?('.en.txt') ? :en : :ru
+    keywords = Zipf.new(text_filename, language).keywords
+    attributes = perceptron_attributes(keywords)
+    class_number = perceptron.recognize(attributes)
+    classes[class_number]
+  end
+
+  private
+    def prepare_train_texts
+      @train_texts_keywords = @train_texts.map do |class_texts|
+        class_texts.map do |filename|
+          language = filename.end_with?('.en.txt') ? :en : :ru
+          {filename: filename, keywords: Zipf.new(filename, language).keywords}
+        end
+      end
+      @common_attributes_list = @train_texts_keywords.map do |class_texts|
+        class_texts.map do |text|
+          Set.new(text[:keywords])
+        end
+      end.flatten.reduce(:+).to_a
+      @train_texts_attributes = @train_texts_keywords.map do |class_texts|
+        class_texts.map do |text|
+          perceptron_attributes(text[:keywords])
+        end
+      end
+    end
+
+    def perceptron_attributes(text_keywords)
+      @common_attributes_list.map{ |keyword_attribute| text_keywords.include?(keyword_attribute) ? 1 : 0 }
+    end
+
+    def build_perceptron
+      perceptron = Perceptron.try_build_by_samples(classes.count, @common_attributes_list.size,
+                                                   @train_texts_attributes)
+      raise StandardError, "Can't build perceptron with this classes." unless perceptron
+      perceptron
+    end
+end
