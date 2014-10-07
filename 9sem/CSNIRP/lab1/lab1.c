7b861a5e217ece2481ce27917c0a42bc6955c6b7
azhi <astzhe@gmail.com>
2014-10-07 17:57:02 +0300

    CSNIRP lab1 part2

--- 9sem/CSNIRP/lab1/lab1.c
+++ 9sem/CSNIRP/lab1/lab1.c
@@ -5,12 +5,40 @@
 #include <windows.h>
 #include <wincrypt.h>
 
-const size_t buf_size = 128;
-const size_t block_size = 8; // 64-bit blocks
-
 enum Mode {
   ENCODE,
   DECODE
+};
+
+struct AlgDescriptor {
+  const char* name;
+  LPCTSTR provider_name;
+  DWORD provider_type;
+  ALG_ID algid;
+  DWORD block_size;
+};
+
+const short ALG_COUNT = 8;
+const struct AlgDescriptor alg_descriptors[] = {
+  {"RC2", MS_ENHANCED_PROV, PROV_RSA_FULL, CALG_RC2, 64 / 8},
+  {"RC4", MS_ENHANCED_PROV, PROV_RSA_FULL, CALG_RC4, 0 / 8},
+  {"DES", MS_ENHANCED_PROV, PROV_RSA_FULL, CALG_DES, 64 / 8},
+  {"2k-3-DES", MS_ENHANCED_PROV, PROV_RSA_FULL, CALG_3DES_112, 64 / 8},
+  {"3k-3-DES", MS_ENHANCED_PROV, PROV_RSA_FULL, CALG_3DES, 64 / 8},
+  {"AES-128", MS_ENH_RSA_AES_PROV, PROV_RSA_AES, CALG_AES_128, 128 / 8},
+  {"AES-192", MS_ENH_RSA_AES_PROV, PROV_RSA_AES, CALG_AES_192, 128 / 8},
+  {"AES-256", MS_ENH_RSA_AES_PROV, PROV_RSA_AES, CALG_AES_256, 128 / 8}
+};
+
+struct AlgDescriptor find_alg_descriptor_by_name(const char* name)
+{
+  struct AlgDescriptor res;
+  for(int i = 0; i < ALG_COUNT; i++)
+    if (strcmp(name, alg_descriptors[i].name) == 0) {
+      res = alg_descriptors[i];
+      break;
+    }
+  return res;
 };
 
 void map_file_into_memory(char* path, DWORD file_access, DWORD file_create,
@@ -53,23 +81,28 @@ HCRYPTKEY generate_key(HCRYPTPROV provider, ALG_ID algid, LPTSTR password) {
   return (res ? key : 0);
 }
 
-DWORD do_crypt(ALG_ID algid, char* password, enum Mode mode,
+DWORD do_crypt(struct AlgDescriptor alg_descriptor, char* password, enum Mode mode,
               PBYTE in_data, PBYTE out_data,
               DWORD in_size, DWORD out_size)
 {
+  size_t block_size = alg_descriptor.block_size;
+  size_t buf_size = block_size != 0 ? block_size : 128;
+
   BOOL res;
   HCRYPTPROV prov;
   DWORD actual_out_size;
 
-  if (!CryptAcquireContext(&prov, 0, MS_ENHANCED_PROV, PROV_RSA_FULL,
+  if (!CryptAcquireContext(&prov, 0, alg_descriptor.provider_name, alg_descriptor.provider_type,
                            CRYPT_VERIFYCONTEXT)) {
-    fprintf(stderr, "Cannot acquire crypt context.");
+    fprintf(stderr, "Cannot acquire crypt context. Error code: %lu\n",
+            GetLastError());
     exit(-1);
   }
 
-  HCRYPTKEY key = generate_key(prov, algid, password);
+  HCRYPTKEY key = generate_key(prov, alg_descriptor.algid, password);
   if (key == 0) {
-    fprintf(stderr, "Cannot generate key.");
+    fprintf(stderr, "Cannot generate key. Error code: %lu\n",
+            GetLastError());
     exit(-1);
   }
 
@@ -121,8 +154,8 @@ void strip_out_file(HANDLE out_file, DWORD actual_out_size)
 
 int main (int argc, char *argv[])
 {
-  if (argc != 5) {
-    fprintf(stderr, "Usage: %s --encode|--decode <password> <input file> <output file>", argv[0]);
+  if (argc != 6) {
+    fprintf(stderr, "Usage: %s --encode|--decode <alg_name> <password> <input file> <output file>", argv[0]);
     return -1;
   }
 
@@ -132,25 +165,27 @@ int main (int argc, char *argv[])
   } else if (strncmp(argv[1], "--decode", 9) == 0) {
     mode = DECODE;
   } else {
-    fprintf(stderr, "Usage: %s --encode|--decode <password> <input file> <output file>", argv[0]);
+    fprintf(stderr, "Usage: %s --encode|--decode <alg_name> <password> <input file> <output file>", argv[0]);
     return -1;
   }
+
+  struct AlgDescriptor alg_descriptor = find_alg_descriptor_by_name(argv[2]);
 
   HANDLE in_file = 0;
   HANDLE in_file_mapping = 0;
   PBYTE in_data = NULL;
   DWORD in_size = 0;
-  map_file_into_memory(argv[3], GENERIC_READ, OPEN_EXISTING, PAGE_READONLY, FILE_MAP_READ, 0,
+  map_file_into_memory(argv[4], GENERIC_READ, OPEN_EXISTING, PAGE_READONLY, FILE_MAP_READ, 0,
                        &in_file, &in_file_mapping, &in_data, &in_size);
 
   HANDLE out_file = 0;
   HANDLE out_file_mapping = 0;
   PBYTE out_data = NULL;
-  DWORD out_size = (in_size / 8 + 2) * 8;
-  map_file_into_memory(argv[4], GENERIC_READ | GENERIC_WRITE, CREATE_ALWAYS, PAGE_READWRITE, FILE_MAP_WRITE, out_size,
+  DWORD out_size = in_size + alg_descriptor.block_size;
+  map_file_into_memory(argv[5], GENERIC_READ | GENERIC_WRITE, CREATE_ALWAYS, PAGE_READWRITE, FILE_MAP_WRITE, out_size,
                        &out_file, &out_file_mapping, &out_data, NULL);
 
-  DWORD actual_out_size = do_crypt(CALG_3DES, argv[2], mode, in_data, out_data, in_size, out_size);
+  DWORD actual_out_size = do_crypt(alg_descriptor, argv[3], mode, in_data, out_data, in_size, out_size);
   strip_out_file(out_file, actual_out_size);
 
   UnmapViewOfFile(in_data);
---
+++ 9sem/CSNIRP/lab1/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+algs=('RC2' 'RC4' 'DES' '2k-3-DES' '3k-3-DES' 'AES-128' 'AES-192' 'AES-256')
+for alg in ${algs[*]}
+do
+  rm -f test_out.txt test_diff.txt
+  echo -ne "$(tput setaf 1)"
+  wine lab1.exe --encode $alg 12345678 test.txt test_out.txt
+  wine lab1.exe --decode $alg 12345678 test_out.txt test_diff.txt
+  cmp test{,_diff}.txt && echo "$(tput sgr0)$(tput setaf 2)$alg is OK$(tput sgr0)"
+done
